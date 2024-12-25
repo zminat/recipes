0f72b83a1cbf55de94730a6d227954bf50937563
Natalia Zmitrichenko <rubanka.ru@gmail.com>
2024-12-25 10:15:14 +0500

    Add Category model

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Category
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description')
+
+
+admin.site.register(Category, CategoryAdmin)
